646c3a93809f79de4e2e6d17e08b737be4cfed45
kevin kaneza <65622766+kevindermot99@users.noreply.github.com>
2024-02-02 17:24:16 +0200

    dark & light theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import { useEffect } from 'react';
 
 function App() {
+
+  useEffect(() => {
+    const SystemThemeUser = localStorage.getItem("appTheme");
+    if (SystemThemeUser === "light") {
+      document.querySelector("body").classList.remove("dark");
+    }
+    // dark
+    else {
+      localStorage.setItem('appTheme', "dark");
+      document.querySelector("body").classList.add("dark");
+    }
+
+    window.matchMedia('(prefers-color-scheme: dark)')
+      .addEventListener('change', ({ matches }) => {
+        if (matches) {
+          // dark
+          document.body.classList.add('dark');
+          localStorage.setItem('appTheme', "dark");
+        } 
+        else {
+          // light
+          document.body.classList.remove('dark');
+          localStorage.setItem('appTheme', "light");
+        }
+      })
+  }, []);
+
   return (
-    <div>App</div>
+    <div>
+      
+    </div>
   )
 }
 
